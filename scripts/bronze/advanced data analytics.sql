f2de1f5f36a7cdd15301bd05375fd51c775f5560
pamuditha <pamudithasenanayake@gmail.com>
2026-02-04 00:14:36 +0530

    cumulative analysis finished

--- scripts/bronze/advanced data analytics.sql
+++ scripts/bronze/advanced data analytics.sql
@@ -62,3 +62,49 @@ WHERE order_date IS NOT NULL
 GROUP BY FORMAT(order_date, 'yyyy-MMM')
 ORDER BY FORMAT(order_date, 'yyyy-MMM')
 
+--cumulative analysis
+
+SELECT 
+order_date,
+total_sales,
+SUM(total_sales) OVER (PARTITION BY order_date ORDER BY order_date) AS running_total_sales
+FROM
+(
+SELECT
+DATETRUNC(month, order_date) AS order_date,
+SUM(sales_amount) AS total_sales
+FROM gold.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY DATETRUNC(month, order_date) 
+)t
+
+
+SELECT 
+order_date,
+total_sales,
+SUM(total_sales) OVER (ORDER BY order_date) AS running_total_sales
+FROM
+(
+SELECT
+DATETRUNC(year, order_date) AS order_date,
+SUM(sales_amount) AS total_sales
+FROM gold.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY DATETRUNC(year, order_date) 
+)t
+
+SELECT 
+order_date,
+total_sales,
+SUM(total_sales) OVER (ORDER BY order_date) AS running_total_sales,
+AVG(avg_price) OVER (ORDER BY order_date) AS moving_average_price
+FROM
+(
+SELECT
+DATETRUNC(year, order_date) AS order_date,
+SUM(sales_amount) AS total_sales,
+AVG(price) AS avg_price
+FROM gold.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY DATETRUNC(year, order_date) 
+)t
